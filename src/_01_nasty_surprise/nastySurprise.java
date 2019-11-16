dd65787c334898c7d56cfb9a3b4a8ff43fb84ee6
League Student <league@iMac-10.attlocal.net>
2019-11-15 18:24:35 -0800

    :smile:

--- src/_01_nasty_surprise/nastySurprise.java
+++ src/_01_nasty_surprise/nastySurprise.java
@@ -1,4 +1,7 @@
 package _01_nasty_surprise;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,32 +10,57 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class nastySurprise {
+public class nastySurprise implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
-	
-public static void main(String[] args) {
-	
+	JButton button2 = new JButton();
 
-showPicture("https://www.google.com/search?q=cute+doggy&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiWyuCGh9zlAhVFMawKHZToAmQQ_AUIEigB&biw=1920&bih=852#imgrc=OZunU8ySS-sYsM:");
+	public static void main(String[] args) {
+		nastySurprise ns = new nastySurprise();
+		ns.run();
 
-	
-}
-	
+	}
+
+	public void run() {
+		panel.add(button);
+		panel.add(button2);
+		frame.add(panel);
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		button.setText("Trick");
+		button2.setText("Treat");
+		frame.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getSource().equals(button)) {
+showPicture("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7sjnenfSOkD5l3dCZBRT86B431qEXB06WCIjtcZmbfH4FTzG4gQ&s");
+		}
+
+		else if (e.getSource().equals(button2)) {
+			showPicture("http://static-23.sinclairstoryline.com/resources/media/d60d4f1c-9fcc-4ae4-8058-05e885080e19-large16x9_momo2.PNG?1551379953599");
+		}
+	}
+
 	private static void showPicture(String imageUrl) {
-	    try {
-	        URL url = new URL(imageUrl);
-	        Icon icon = new ImageIcon(url);
-	        JLabel imageLabel = new JLabel(icon);
-	        JFrame frame = new JFrame();
-	        frame.add(imageLabel);
-	        frame.setVisible(true);
-	        frame.pack();
-	    } catch (MalformedURLException e) {
-	        e.printStackTrace();
-	    }
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 }
